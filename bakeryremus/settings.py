fa2bdbdeba3b4dbf897e40cb3f5a8a6aff10da05
totalnoMartina <totalnobzvz@yahoo.com>
2022-11-27 13:32:12 +0000

    Add Procfile - deleted - add new one

--- bakeryremus/settings.py
+++ bakeryremus/settings.py
@@ -41,6 +41,9 @@ if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
+CSRF_TRUSTED_ORIGINS = ['bakery-remus-sirghe.herokuapp.com', ]
+
+
 # Application definition
 
 INSTALLED_APPS = [
