a659f281fbcc0d922c415f3ca5622b0fb1e6e867
totalnoMartina <totalnobzvz@yahoo.com>
2023-01-15 15:33:08 +0000

    Add view for about us page

--- oaksmokebakery/views.py
+++ oaksmokebakery/views.py
@@ -5,3 +5,9 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'oaksmokebakery/index.html')
+
+
+def aboutus(request):
+    """ A view to return the about page """
+
+    return render(request, 'oaksmokebakery/about.html')
