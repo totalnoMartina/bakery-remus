eeb38e80aff38ef195d7d7361387471831702f1c
totalnoMartina <totalnobzvz@yahoo.com>
2023-01-17 20:10:59 +0000

    Add price into items model, rendering on html

--- oaksmokebakery/admin.py
+++ oaksmokebakery/admin.py
@@ -5,3 +5,12 @@ from .models import Item, OrderCart, OrderItem
 admin.site.register(Item)
 admin.site.register(OrderCart)
 admin.site.register(OrderItem)
+
+class OrderItemAdmin(admin.ModelAdmin):
+    """ An admin panel for viewing macrame objects """
+    list_display = (
+        'item',
+        'price',
+    )
+
+    ordering = ('-item',)
--- oaksmokebakery/models.py
+++ oaksmokebakery/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Item(models.Model):
     title = models.CharField(max_length=100)
+    price = models.FloatField(blank=True, null=True)
 
 
     def __str__(self):
@@ -16,7 +17,7 @@ class OrderItem(models.Model):
     price = models.FloatField()
 
     def __str__(self):
-        return self.title
+        return self.title, self.price
     
 
 class OrderCart(models.Model):
