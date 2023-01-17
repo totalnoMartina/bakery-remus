9d45400c35ba97d48e20b5203c3422ae8903eeca
totalnoMartina <totalnobzvz@yahoo.com>
2023-01-17 21:09:16 +0000

    Add categories to items

--- oaksmokebakery/models.py
+++ oaksmokebakery/models.py
@@ -1,18 +1,30 @@
 from django.conf import settings
 from django.db import models
 
+CATEGORIES = [
+    ('Bread', 'Bread'),
+    ('Cake', 'Cake'),
+    ('Pastries', 'Pastries'),
+    ('Specialty Bread', 'Specialty Bread'),
+    ('Ocassion Cake', 'Ocassion Cake'),
+]
+
 
 class Item(models.Model):
+    """ A model for structure of items """
     title = models.CharField(max_length=100)
     price = models.FloatField(blank=True, null=True)
+    category = models.CharField(choices=CATEGORIES, max_length=25, null=True)
 
 
     def __str__(self):
+        """ To present the title of item """
         return self.title
     
 
 
 class OrderItem(models.Model):
+    """ Structure for the items order """
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     price = models.FloatField()
 
